31728e9ed58751d453f78cf1b7358934dfe08dfb
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-06-13 16:51:52 +0530

    Code documentation using Compodoc

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,28 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+/**
+ * CounterComponent
+ */
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
+
+  /**
+   * Intial count is set to 0
+   */
   public count: number = 0;
 
+  /**
+   * constructor is put into use to inject dependencies into the component class.
+   */
   constructor() { }
 
+  /**
+   * ngOnInit is invoked only once when the directive is instantiated.
+   */
   ngOnInit(): void {
   }
 
+  /**
+   * increment method
+   * @returns number
+   */
   increment(): number {
     this.count += 1;
     return this.count;
   }
 
+  /**
+   * decrement method
+   * @returns number
+   */
   decrement(): number {
     this.count -= 1;
     return this.count;
   }
 
+  /**
+   * reset method
+   * @returns number
+   */
   reset(): number {
     this.count = 0;
     return this.count;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,9 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/**
+ * Dev environment
+ */
 export const environment = {
   production: false
 };
